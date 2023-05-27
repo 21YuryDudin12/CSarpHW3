4770cbc6f7761c433807e5256c5bd48e3a753c33
Yury Dudin <cepob33@gmail.com>
2023-05-27 12:32:23 +0500

    ex23 complite

---
+++ Ex23/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число N, программа укажет все кубы от 0 до N");
+int n = int.Parse(Console.ReadLine());
+
+void FindQubeDigit (int nf)
+{
+int i = 0;
+for (i=0; i < (nf + 1); i++)
+{
+    int f = i;
+    f = f*f*f;
+    if (f == 0)
+    {
+        
+    }
+    else
+    {
+         Console.WriteLine(f);
+    }
+}
+}
+FindQubeDigit(n);
