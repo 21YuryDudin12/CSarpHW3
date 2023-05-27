789d2c70538940a897b69e360bb356bb65d798e2
Yury Dudin <cepob33@gmail.com>
2023-05-27 12:31:30 +0500

    ex 19 complite

---
+++ Ex19/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход 
+//пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите 5-ти значное число, программа проверит, является ли оно палиндромом");
+void CheckPalindrom(int digit)
+{
+    int First2 = digit % 100;
+    int Sec2 = digit / 1000;
+    Sec2 = ((Sec2 % 10)*10) + (Sec2 / 10);
+    if (First2 == Sec2)
+    {
+        Console.WriteLine("Да");
+    }
+    else 
+    {
+        Console.WriteLine("Нет");
+    }
+}
+int Num = int.Parse(Console.ReadLine());
+if (Num < 1000 || Num > 99999)
+{
+    Console.WriteLine("Это не пятизначное число");
+}
+else
+{
+    CheckPalindrom(Num);
+}
